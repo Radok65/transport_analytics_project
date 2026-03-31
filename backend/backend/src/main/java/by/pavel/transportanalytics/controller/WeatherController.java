14f36e3a54275a45dcfa02dca3a3dbc47052490d
Radok65 <pavel.shiltsev@gmail.com>
2026-03-31 13:49:38 +0300

    v2.3 weather

---
+++ backend/backend/src/main/java/by/pavel/transportanalytics/controller/WeatherController.java
@@ -0,0 +1,25 @@
+package by.pavel.transportanalytics.controller;
+
+import by.pavel.transportanalytics.service.WeatherService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/weather")
+@CrossOrigin(origins = "http://localhost:3000") // Разрешаем запросы с фронта
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping
+    public ResponseEntity<WeatherService.WeatherData> getWeather(
+            @RequestParam Double lat,
+            @RequestParam Double lon) {
+        WeatherService.WeatherData weather = weatherService.getWeatherAtLocation(lat, lon);
+        return ResponseEntity.ok(weather);
+    }
+}
